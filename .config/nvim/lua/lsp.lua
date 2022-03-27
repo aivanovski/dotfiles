296def4f210b9a3da0127d2b17957054e674c26e
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-03-27 20:40:29 +0200

    Update nvim config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -35,7 +35,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'solargraph', 'kotlin_language_server', 'clojure_lsp' }
+local servers = { 'solargraph', 'kotlin_language_server', 'clojure_lsp', 'bashls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
